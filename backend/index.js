4cc37a0e7a5508ada9b1a60d60b4fdbbe32225da
Josh Rimes - Desktop <joshjohn1104@gmail.com>
2025-09-08 22:12:27 +0100

    Update: added watch routes & enhanced server setup

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,22 @@
-const express = require("express");
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import watchRoutes from "./routes/watchRoutes.js";
+
+dotenv.config();
+
 const app = express();
+app.use(cors());
 app.use(express.json());
+
+// Routes
+app.use("/api/watches", watchRoutes);
 
 app.get("/", (req, res) => {
     res.send("IQueue backend is running!");
 });
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server is listening on ${PORT}.`));
+app.listen(PORT, () => {
+    console.log(`Server is listening on ${PORT}.`)
+});
