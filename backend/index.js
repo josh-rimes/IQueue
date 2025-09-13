736212c95e2e93bc2d3f380ce6668db0d78715a6
Josh Rimes - Desktop <joshjohn1104@gmail.com>
2025-09-13 15:52:33 +0100

    Add: authRoutes to index.js

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import watchRoutes from "./routes/watchRoutes.js";
+import authRoutes from "./routes/authRoutes.js";
 import { runWorker } from "./worker.js";
 
 dotenv.config();
@@ -12,6 +13,7 @@ app.use(express.json());
 
 // Routes
 app.use("/api/watches", watchRoutes);
+app.use("api/auth", authRoutes);
 
 app.get("/", (req, res) => {
     res.send("IQueue backend is running!");
